b166fdd9a62e74875f00424bf70bc18e97a656cb
AndreBr0 <makovskiy.ande5910@gmail.com>
2019-01-30 20:48:25 +0300

    russian в 4 уроке

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -377,7 +377,7 @@ fun russian(n: Int): String {
             otv = slovo6 + slovo5 + slovo4 + "тысяч " + slovo3 + slovo2 + slovo1
         } else otv = slovo6 + slovo5 + slovo4 + "тысяч"
     }
-    if ((ne5 == 1) || (ne4 != 0)) {
+    if ((ne5 == 1) && (ne4 != 0)) {
         if (slovo7 != "") {
             otv = slovo6 + slovo5 + "тысяч " + slovo3 + slovo2 + slovo1
         } else otv = slovo6 + slovo5 + "тысяч"
